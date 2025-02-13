ca3d05f8d51e1b297b5f7c9b875fac6a978a45f2
Miska Poyry <bhd692@myy.haaga-helia.fi>
2025-02-13 16:27:27 +0200

    Formatoi koodia prettierilla seka laita kommentteja mahdollisimman paljon

--- scripts.js
+++ scripts.js
@@ -1,89 +1,91 @@
-function sendFakeMail(){
+function sendFakeMail() {
+  /* HAE EMAIL JA MESSAGE MUUTTUJAT KOODISTA */
 
-    /* HAE EMAIL JA MESSAGE MUUTTUJAT KOODISTA */
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
 
-    let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
+  /* TARKISTA ONKO EMAIL TAI MESSAGE EMPTY, JOS ON ALERTAA */
 
-    /* TARKISTA ONKO EMAIL TAI MESSAGE EMPTY, JOS ON ALERTAA */
+  if (email == "" || message == "") {
+    bootstrapAlert("Täytä kaikki kentät", "danger");
+    return;
+  }
 
-    if (email == '' || message == '') {
-        bootstrapAlert('Täytä kaikki kentät', 'danger');
-        return;
-    }
+  /* NÄYTÄ FAKE ALERT, ETTÄ VIESTI OLISI LÄHETETTY */
 
-    /* NÄYTÄ FAKE ALERT, ETTÄ VIESTI OLISI LÄHETETTY */
+  bootstrapAlert("Viestisi on lähetetty!", "success");
 
-    bootstrapAlert('Viestisi on lähetetty!', 'success');
+  /* TYHJENNÄ MUUTTUJAT */
 
-    /* TYHJENNÄ MUUTTUJAT */
-
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
 }
 
 /* TEE BOOTSTRAPILLA ALERT */
 
 function bootstrapAlert(message, color) {
-
-/* ETSI OIKEA KOHTA KOODISTA, JOHON ALERT TULEE */
+  /* ETSI OIKEA KOHTA KOODISTA, JOHON ALERT TULEE */
   let mailAlerts = document.getElementById("mailAlerts");
 
   /* LUO ALERT BOOTSTRAPILLA */
 
   let alertBootstrap = `
         <div class="alert alert-${color} show" role="alert">
-            ${color === 'success' ? 'Onnistui!' : 'Virhe!'} ${message}
+            ${color === "success" ? "Onnistui!" : "Virhe!"} ${message}
         </div>
     `;
 
-    /* LISÄÄ ALERT innerHTML, JOLLOIN SE MENEE HTML */
+  /* LISÄÄ ALERT innerHTML, JOLLOIN SE MENEE HTML */
 
-    mailAlerts.innerHTML = alertBootstrap;
+  mailAlerts.innerHTML = alertBootstrap;
 
-    /* TYHJENNÄ innerHTML 3 SEKUNNIN JÄLKEEN */
+  /* TYHJENNÄ innerHTML 3 SEKUNNIN JÄLKEEN */
 
-    setTimeout(() => {
-        mailAlerts.innerHTML = '';
-    }, 3000);
+  setTimeout(() => {
+    mailAlerts.innerHTML = "";
+  }, 3000);
 }
 
 /* NÄYTÄ KAIKKI CLASSILLA TYPING ANIMOIDUT TEKSTIT KERRAN KÄYTTÄJÄN NÄHDESSÄ ELEMENTIN */
 
 document.addEventListener("DOMContentLoaded", function () {
-    
-    // ETSI KAIKKI TYPING CLASSIT
-    let typings = document.querySelectorAll(".typing");
+  // ETSI KAIKKI TYPING CLASSIT
+  let typings = document.querySelectorAll(".typing");
 
-    //MIKÄLI NÄYTTÄ ON MOBIILI, NIIN LAITA TEKSTI NÄKYMÄÄN ILMAN ANIMAATIOTA
-    if (window.innerWidth < 768) {
-        typings.forEach(element => {
-            element.style.width = "100%";
-            element.style.whiteSpace = "normal";
-        });
-        return;
-    }
+  //MIKÄLI NÄYTTÄ ON MOBIILI, NIIN LAITA TEKSTI NÄKYMÄÄN ILMAN ANIMAATIOTA
+  if (window.innerWidth < 768) {
+    typings.forEach((element) => {
+      element.style.width = "100%";
+      element.style.whiteSpace = "normal";
+    });
+    return;
+  }
 
-    // KÄYTÄ INTERSECTION OBSERVERIA, JOTTA NÄHDÄÄN KUN ELEMENTTI ON NÄKYVISSÄ
-    let observer = new IntersectionObserver(entries => {
-        // FOR LOOPPAA JOKAINEN LÖYDETTY TYPING ELEMENTTI
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // MUUTA ANIMAATIO CSS NONESTA NIIN, ETTÄ SE NÄKYY JA TOIMII
-                entry.target.style.animation = "typing 5s steps(100, end) forwards";
-                // POISTA OBSERVAUS, JOTTA ANIMAATIOT NÄKYY VAIN KERRAN
-                observer.unobserve(entry.target);
-            }
-        });
-        // VAATII 60% NÄKYVYYDEN, JOTTA ANIMAATIO ALKAA
-    }, { threshold: 0.6 });
+  // KÄYTÄ INTERSECTION OBSERVERIA, JOTTA NÄHDÄÄN KUN ELEMENTTI ON NÄKYVISSÄ
+  let observer = new IntersectionObserver(
+    (entries) => {
+      // FOR LOOPPAA JOKAINEN LÖYDETTY TYPING ELEMENTTI
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // MUUTA ANIMAATIO CSS NONESTA NIIN, ETTÄ SE NÄKYY JA TOIMII
+          entry.target.style.animation = "typing 5s steps(100, end) forwards";
+          // POISTA OBSERVAUS, JOTTA ANIMAATIOT NÄKYY VAIN KERRAN
+          observer.unobserve(entry.target);
+        }
+      });
+      // VAATII 60% NÄKYVYYDEN, JOTTA ANIMAATIO ALKAA
+    },
+    { threshold: 0.6 }
+  );
 
-    typings.forEach(element => observer.observe(element));
+  typings.forEach((element) => observer.observe(element));
 });
 
 function sendFakeFeedback() {
   let freeFeedback = document.getElementById("freeFeedback").value.trim();
-  let selectedRadio = document.querySelector('input[name="inlineRadioOptions"]:checked');
+  let selectedRadio = document.querySelector(
+    'input[name="inlineRadioOptions"]:checked'
+  );
 
   // Tarkista onko feedback tai radiot empty
   if (freeFeedback === "" || selectedRadio === null) {
@@ -106,7 +108,11 @@ function feedbackAlert(message, color) {
   /* LUO ALERT BOOTSTRAPILLA */
   let alertBootstrap = `
       <div class="alert alert-${color} show" role="alert">
-          ${color === "success" ? "Lähettäminen onnistui" : "Lähettäminen epäonnistui!"} ${message}
+          ${
+            color === "success"
+              ? "Lähettäminen onnistui"
+              : "Lähettäminen epäonnistui!"
+          } ${message}
       </div>
   `;
 
